113c43d1f4239b61ad5efaf39a218302c9d58f34
Terrngu <terrngu@gmail.com>
2022-10-11 21:21:45 -0600

    Refined code and added assets

--- CS408_AS2/Assets/CSVFileReader.cs
+++ CS408_AS2/Assets/CSVFileReader.cs
@@ -8,34 +8,59 @@ public class CSVFileReader : MonoBehaviour
 {
 
     public GameObject cubeTest;
+    public GameObject cylinderTest;
     public TextAsset csvFile;
+    public int i = 1;
 
     // Start is called before the first frame update
-    //void Start()
-    //{   
-    //}
+    void Start()
+    {   
+        InvokeRepeating("readCSV", 1f, 1f);  //1s delay, repeat every 1s
+    }
 
     // Update is called once per frame
     void Update()
     {
-        readCSV();
+
+        //readCSV();
+
     }
 
     void readCSV(){
+
+        Debug.Log(Time.time);
+
         string [] records = csvFile.text.Split('\n');
-        for (int i = 1; i < records.Length; i++){
-            string [] fields = records[i].Split(',');
+        //for (int i = 1; i < records.Length; i++){
+        string [] fields = records[i].Split(',');
 
-            //Update position
-            Vector3 newpos = new Vector3(0,0,0);
-            cubeTest.transform.position = newpos;
+            if(float.Parse(fields[0]) == 0 ){
+                //Update position
+                Vector3 newpos = new Vector3(float.Parse(fields[2]), float.Parse(fields[3]), float.Parse(fields[4]));
+                cubeTest.transform.position = newpos;
 
-            //Update rotation
-            cubeTest.transform.Rotate(float.Parse(fields[1]), float.Parse(fields[2]), float.Parse(fields[3]));
+                //Update rotation
+                cubeTest.transform.Rotate(float.Parse(fields[5]), float.Parse(fields[6]), float.Parse(fields[7]));
 
-            //Update scale
-            Vector3 newscale = new Vector3(0,0,0);
-            cubeTest.transform.localScale += newscale;
+                //Update scale
+                Vector3 newscale = new Vector3(float.Parse(fields[8]), float.Parse(fields[9]), float.Parse(fields[10]));
+                cubeTest.transform.localScale += newscale;
+
+                i++;
+            }
+
+            //if(float.Parse(fields[0]) == 1 ){
+                //Update position
+            //    Vector3 newpos = new Vector3(float.Parse(fields[2]), float.Parse(fields[3]), float.Parse(fields[4]));
+            //    cylinderTest.transform.position = newpos;
+
+                //Update rotation
+            //    cylinderTest.transform.Rotate(float.Parse(fields[5]), float.Parse(fields[6]), float.Parse(fields[7]));
+
+                //Update scale
+            //    Vector3 newscale = new Vector3(float.Parse(fields[8]), float.Parse(fields[9]), float.Parse(fields[10]));
+            //    cylinderTest.transform.localScale += newscale;
+            //}
         }
-    }
+    //}
 }
